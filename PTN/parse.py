dd2218974a43bf82156e3ff8ad66e08169c96502
Giorgio Momigliano <gmomigliano@protonmail.com>
2020-07-04 16:02:07 +0200

    Simplify excess logic

--- PTN/parse.py
+++ PTN/parse.py
@@ -7,29 +7,25 @@ from .extras import exceptions, patterns_ignore_title, link_patterns
 
 class PTN(object):
     def __init__(self):
-        self.excess_raw = None
         self.torrent_name = None
         self.parts = None
         self.part_slices = None
         self.match_slices = None
+        self.standardise = None
 
-        self.post_title_pattern = '(?:{}|{})'.format(link_patterns(patterns['season'])
-                                                     , link_patterns(patterns['year']))
+        self.post_title_pattern = '(?:{}|{})'.format(link_patterns(patterns['season']),
+                                                     link_patterns(patterns['year']))
 
-    # Ignored patterns will still remove their match from excess.
-    def _part(self, name, match_slice, raw, clean, overwrite=False):
+    def _part(self, name, match_slice, clean, overwrite=False):
         if overwrite or name not in self.parts:
             if isinstance(clean, list) and len(clean) == 1:
                 clean = clean[0]  # Avoids making a list if it only has 1 element
             self.parts[name] = clean
             self.part_slices[name] = match_slice
 
-        if name != 'excess':
-            # The instructions for adding excess
-            if not match_slice and raw:
-                self.excess_raw = self.excess_raw.replace(raw, '', 1)
-            elif match_slice:
-                self.match_slices.append(match_slice)
+        # Ignored patterns will still be considered 'matched' to remove them from excess.
+        if match_slice:
+            self.match_slices.append(match_slice)
 
     @staticmethod
     def _clean_string(string):
@@ -47,8 +43,8 @@ class PTN(object):
         self.parts = dict()
         self.part_slices = dict()
         self.torrent_name = name
-        self.excess_raw = name
         self.match_slices = list()
+        self.standardise = standardise
 
         for key, pattern_options in [(key, patterns[key]) for key in patterns_ordered]:
             pattern_options = self.normalise_pattern_options(pattern_options)
@@ -73,19 +69,17 @@ class PTN(object):
                 match = matches[match_index]['match']
                 match_start, match_end = matches[match_index]['start'], matches[match_index]['end']
                 if key in self.parts:  # We can skip ahead if we already have a matched part
-                    self._part(key, (match_start, match_end),
-                               match[0], None, overwrite=False)
+                    self._part(key, (match_start, match_end), None, overwrite=False)
                     continue
 
                 index = self.get_match_indexes(match)
 
-                # patterns for multiseason/episode make the range, and only the range, appear in match[0]
-                if (key == 'season' or key == 'episode') and index['clean'] == 0:
+                if key == 'season' or key == 'episode':
                     clean = self.get_season_episode(match)
                 elif key == 'language':
                     clean = self.get_language(match)
                 elif key == 'subtitles':
-                    clean = self.get_subtitles(match, match_start)
+                    clean = self.get_subtitles(match)
                 elif key in types.keys() and types[key] == 'boolean':
                     clean = True
                 else:
@@ -93,23 +87,20 @@ class PTN(object):
                     if key in types.keys() and types[key] == 'integer':
                         clean = int(clean)
 
-                if standardise:
+                if self.standardise:
                     clean = self.standardise_clean(clean, key, replace, transforms)
 
-                self._part(key, (match_start, match_end),
-                           match[index['raw']], clean)
-
-        self.merge_match_slices()
+                self._part(key, (match_start, match_end), clean)
 
         self.process_title()
         self.fix_known_exceptions()
 
-        unmatched = self.process_unmatched()
+        unmatched = self.get_unmatched()
         for f in post_processing_before_excess:
             unmatched = f(self, unmatched)
 
-        if unmatched:
-            self._part('excess', None, unmatched, self.clean_unmatched())
+        # clean_unmatched() depends on the before_excess methods adding more match slices.
+        self._part('excess', None, self.clean_unmatched())
 
         for f in post_processing_after_excess:
             f(self)
@@ -161,8 +152,8 @@ class PTN(object):
             else:
                 m = [m]
             parsed_matches.append({'match': m,
-                                    'start': match[1],
-                                    'end': match[2]})
+                                   'start': match[1],
+                                   'end': match[2]})
         return parsed_matches
 
     # Only use part of the torrent name after the (guessed) title (split at a season or year)
@@ -197,8 +188,6 @@ class PTN(object):
 
     @staticmethod
     def get_season_episode(match):
-        # handle multi season/episode
-        # e.g. S01-S09
         clean = None
         m = re.findall(r'[0-9]+', match[0])
         if m and len(m) > 1:
@@ -215,25 +204,18 @@ class PTN(object):
 
         return clean
 
-    def get_subtitles(self, match, match_start):
+    @staticmethod
+    def get_subtitles(match):
         # handle multi subtitles
         m = re.split(r'{}+'.format(delimiters), match[0])
         m = list(filter(None, m))
         clean = list()
-        for x in m:
-            if len(m) == 1 and re.match('subs?', x, re.I):
-                clean.append(x)
-            elif not re.match('subs?|soft', x, re.I):
-                clean.append(x)
-
-        # If this match starts like the language one did, the only match for language
-        # and subtitles is a list of langs directly followed by a subs-string. When this
-        # is true, they would both match on it, but what it likely means is that all the
-        # langs are language, and the subs string just indicates the existance of subtitles.
-        # (e.g. Ita.Eng.MSubs would match Ita and Eng for language and subs - this makes
-        # subs only become MSubs, and leaves language as Ita and Eng)
-        if 'language' in self.part_slices and self.part_slices['language'][0] == match_start:
-            clean = clean[-1]
+        if len(m) == 1:
+            clean = m
+        else:
+            for x in m:
+                if not re.match('subs?|soft', x, re.I):
+                    clean.append(x)
 
         return clean
 
@@ -256,7 +238,8 @@ class PTN(object):
         cleaned_langs = list()
         for lang in clean:
             for (lang_regex, lang_clean) in langs:
-                if re.match(lang_regex, re.sub(link_patterns(patterns['subtitles'][2:]), '', lang, flags=re.I), re.IGNORECASE):
+                if re.match(lang_regex, re.sub(link_patterns(patterns['subtitles'][2:]), '', lang, flags=re.I),
+                            re.IGNORECASE):
                     cleaned_langs.append(lang_clean)
                     break
         clean = cleaned_langs
@@ -293,31 +276,30 @@ class PTN(object):
             # (or more likely got parsed as something else), as no torrents have it that
             # far away from the beginning of the release title.
             if title_start > sorted(self.part_slices.values(), key=lambda s: s[0])[min(3, len(self.part_slices)-1)][0]:
-                self._part('title', None, '', '')
+                self._part('title', None, '')
 
             raw = self.torrent_name[title_start:title_end]
             if '(' in raw:
                 title_end = self.torrent_name.index('(', title_start)
                 raw = raw.split('(')[0]
             clean = self._clean_string(raw)
-            self._part('title', (title_start, title_end), raw, clean)
-            self.merge_match_slices()
+            self._part('title', (title_start, title_end), clean)
         else:
-            self._part('title', None, '', '')
+            self._part('title', None, '')
 
     def unmatched_list(self, keep_punctuation=True):
-        self.merge_match_slices()  # TODO look at removing this around
+        self.merge_match_slices()
         unmatched = list()
         prev_start = 0
         end = len(self.torrent_name)  # A default so the last append won't crash if nothing has matched
         # Find all unmatched strings that aren't just punctuation
         for (start, end) in self.match_slices:
-            if keep_punctuation or not re.match(delimiters + '*\Z', self.torrent_name[prev_start:start]):
+            if keep_punctuation or not re.match(delimiters + r'*\Z', self.torrent_name[prev_start:start]):
                 unmatched.append((prev_start, start))
             prev_start = end
 
         # Add the last unmatched slice
-        if keep_punctuation or not re.match(delimiters + '*\Z', self.torrent_name[end:]):
+        if keep_punctuation or not re.match(delimiters + r'*\Z', self.torrent_name[end:]):
             unmatched.append((end, len(self.torrent_name)))
 
         return unmatched
@@ -328,18 +310,16 @@ class PTN(object):
         for exception in exceptions:
             incorrect_key, incorrect_value = exception['incorrect_parse']
             if (self.parts['title'] == exception['parsed_title'] and
-               incorrect_key in self.parts and self.parts[incorrect_key] == incorrect_value):
+                incorrect_key in self.parts and self.parts[incorrect_key] == incorrect_value):
                 self.parts.pop(incorrect_key)
-                self._part('title', None, exception['actual_title'], exception['actual_title'], overwrite=True)
+                self._part('title', None, exception['actual_title'], overwrite=True)
 
-    # TODO pretty sure this and self.excess_raw aren't really needed.
-    def process_unmatched(self):
-        shift = 0
-        for (start, end) in self.match_slices:
-            self.excess_raw = self.excess_raw[:start-shift] + self.excess_raw[end-shift:]
-            shift += end - start
+    def get_unmatched(self):
+        unmatched = ''
+        for (start, end) in self.unmatched_list():
+            unmatched += self.torrent_name[start:end]
 
-        clean = re.sub(r'(^[-. ()]+)|([-. ]+$)', '', self.excess_raw)
+        clean = re.sub(r'(^[-. ()]+)|([-. ]+$)', '', unmatched)
         return re.sub(r'[()/]', ' ', clean)
 
     def clean_unmatched(self):
